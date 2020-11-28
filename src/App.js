ff378151ee39320a7be76c547c6adb112a6859ab
JLF <32518279+JoseLuisFriedrich@users.noreply.github.com>
2020-11-28 11:31:59 -0300

    JLF

--- src/App.js
+++ src/App.js
@@ -69,3 +69,25 @@ function App() {
 }
 
 export default App
+
+// 00:00:00 ✔️ Liberías para darle estilo al sitio (Boostrap, Material UI, Styled Components, SCSS, Font-Awesome)
+// 00:07:00 ✔️ Repaso de promesas y filtrar elemento de array de productos.
+// 00:15:00 ✔️ Resolviendo problema de utilización de la api https://my-json-server.typicode.com/ para hacer una fake api con datos propios
+// 00:24:00 ✔️ Link to a cada producto para hacer la redirección al detalle correspondiente, BrowserRouter
+//          💩 (sólo Home e ItemDetailContainer van en /containers. El resto en /components, no hacer como yo que metí todo en containers)
+//          👀 (lo acomodé en mi GitHub)
+// 00:50:00 ✔️ Explicación del desafío de useContext, props de evento para tener el botón comprar en ItemDetail y recibir el counter de ItemCount
+// 00:57:00 ✔️ creación de context, useContext con useState Texto, consumir contexto
+// 01:08:00 ✔️ El custom hook más sencillo del mundo para simplificar el consumo de AppContext en componentes. Creando "useAppContext"
+// 01:22:00 ✔️ useContext con useState Texto, implementación de función para setear Texto
+// 01:26:00 💪 useContext con useState Array, implementación de función para setear Array (¡importante!)
+// 01:33:00 ✔️ useContext con useState Object, implementación de función para setear Object
+
+///////////
+// Extra //
+///////////
+
+// 01:46:00 💪 Functional Updates -> Seteando states Array y Object como un "pro react developer"
+// 01:57:00 ✔️ Cómo se implementaría un array de objetos para agregar varios objetos a una lista
+//          👀 (En 02:02:00) copié handleDummyObject, pero tenía que copiar handleDummyArrayObject
+// 02:03:00 🍺 Cómo consumir api de Twitter con Python y cómo podría implementarse en JavaScript
